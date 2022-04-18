194251287a8a4113d0a664bb3dbf445b8a48d63b
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-04-18 17:44:08 +0530

    Add files via upload

---
+++ R/Analysis_cova.R
@@ -0,0 +1,32 @@
+input <- mtcars[,c("am","mpg","hp")]
+print(head(input))
+
+
+# Get the dataset.
+input <- mtcars
+
+# Create the regression model.
+result <- aov(mpg~hp*am,data = input)
+print(summary(result))
+
+
+#Model without interaction between categorical variable and predictor variable
+#Live Demo
+# Get the dataset.
+input <- mtcars
+
+# Create the regression model.
+result <- aov(mpg~hp+am,data = input)
+print(summary(result))
+
+
+
+# Get the dataset.
+input <- mtcars
+
+# Create the regression models.
+result1 <- aov(mpg~hp*am,data = input)
+result2 <- aov(mpg~hp+am,data = input)
+
+# Compare the two models.
+print(anova(result1,result2))
---
+++ R/Arithmetic_op.R
@@ -0,0 +1,43 @@
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v+t)
+
+#[1] 10.0  8.5 10.0
+
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v-t)
+
+#[1] -6.0  2.5  2.0
+
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v*t)
+
+#[1] 16.0 16.5 24.0
+
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v/t)
+
+#[1] 0.250000 1.833333 1.500000
+
+v <- c( 20,5.5,6)
+t <- c(8, 3, 4)
+print(v%%t)
+
+#[1] 4.0 2.5 2.0
+#Give the remainder of the first vector with the second
+
+v <- c( 20,5.5,6)
+t <- c(8, 3, 4)
+print(v%/%t)
+
+#[1] 2 1 1
+#The result of division of first vector with second (quotient)
+
+v <- c( 2,5.5,6)
+t <- c(8, 3, 4)
+print(v^t)
+
+#[1] 2.56000e+10 1.66375e+02 1.29600e+03
